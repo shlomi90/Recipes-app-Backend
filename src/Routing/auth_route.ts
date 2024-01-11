2e111ea3e86220ce89954661f019b63b75d2872f
shlomi90 <106813538+shlomi90@users.noreply.github.com>
2024-01-11 14:25:30 +0200

    Update auth_route.ts

--- src/Routing/auth_route.ts
+++ src/Routing/auth_route.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 const router = express.Router()
 import Auth from '../Controllers/auth_controller';
+import Autnticate from '../Common/auth_middleware';
 
-router.post('/register', Auth.register);
+router.post('/register', );
 
-router.post('/login', Auth.login);
+router.post('/login', );
 
-router.post('/logout', Auth.logout);
+router.get('/logout',Autnticate, Auth.logout);
+
+router.get('/refresh', Auth.refresh);
 
 export default router;
